02a1d6c82413e8615d2de3b9fca990f8d8ec08e3
takeuchima <masato.takeuchi@scopenext.jp>
2022-07-08 03:33:39 +0900

    typescript test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import { Practice1 } from './practice/practice1';
+import { Practice2 } from './practice/practice2';
+import { Practice3 } from './practice/practice3';
 
 function App() {
   return (
     <div className="App">
-      <h1>Start React TypeScript Sample Project</h1>
-      <p>Let's develop with React!</p>
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
     </div>
   );
 }
